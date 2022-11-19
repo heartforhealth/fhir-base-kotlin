a5ccf9c13ccea4cd1dd077f1f96a42a5a570fb7f
Andrei <a.popa@heartforhealth.com>
2022-11-19 17:43:22 +0200

    replaced GCP Artifact Registry with GitHub Packages

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -11,6 +11,7 @@
 group = "io.h4h"
 version = "0.1.4"
 
+
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
@@ -19,8 +20,6 @@ plugins {
     `java-library`
     // Publish to Maven repository
     `maven-publish`
-    // Publish to GCP Artifact Registry repository
-    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.1.4"
 
     // Kotlinx Serialization
     kotlin("plugin.serialization") version "1.7.10"
@@ -42,12 +41,19 @@ publishing {
         }
     }
 
-    // publish on GCP Artifact Registry
+
     repositories {
+        // publish on GitHub Packages
         maven {
-            url = uri("artifactregistry://europe-west4-maven.pkg.dev/ehealth-development/java-repository-1")
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/heartforhealth/fhir-base-kotlin")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
         }
     }
+
 }
 
 dependencies {
